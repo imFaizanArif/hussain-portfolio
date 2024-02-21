9a6e6eda2b78ac35c43a2d444ba3b33caafa59f0
imFaizanArif <faizanarif1245@gmail.com>
2024-02-21 19:05:11 +0500

    loader added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@ import CaseStudies from "@/components/CaseStudies/CaseStudies";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import { Suspense } from "react";
-import Loading from "./loading";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Suspense fallback={<Loading />}>
-        <Hero />
-      </Suspense>
+      <Hero />
       <CaseStudies />
       <Footer />
     </>
